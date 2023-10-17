4488092952251a0769bb89bbdc5bd0445c3d4467
TAZIReda <tazir_reda@outlook.fr>
2023-10-17 11:15:41 +0100

    create models

---
+++ src/app/shared/models/cartItem.ts
@@ -0,0 +1,12 @@
+import { Food } from "./food";
+
+export class CartItem{
+    food: Food;
+    quantity: number=1;
+    constructor(food:Food){ 
+        this.food = food;
+    }
+    get price(): number{
+        return this.food.price*this.quantity;
+    }
+}
